7ff8e31b98663eac964f8e793198f5d634c54fc0
Fachrel Ardaffa Putra <152086322+ardaffa11@users.noreply.github.com>
2024-12-02 16:49:03 +0700

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pd
-import matplotlib.pyplot as plt
+import matplotlib as plt
 
 # Load the data
 top_5_products = pd.read_csv('top_5_products.csv')  # Ganti path sesuai file Anda
